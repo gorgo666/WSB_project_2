3aa5ee230a048744616887da0dd12db8dd15794d
gorgo666 <grzegorz.wiecek.sluzbowy@gmail.com>
2020-05-30 17:25:59 +0200

    add all pages with routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Education from './pages/Education';
+import Courses from './pages/Courses';
+import Hobby from './pages/Hobby';
+import Work from './pages/Work';
+import Contact from './pages/Contact';
+import Nav from './components/Nav';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/education' component={Education} />
+          <Route path='/courses' component={Courses} />
+          <Route path='/hobby' component={Hobby} />
+          <Route path='/work' component={Work} />
+          <Route path='/contact' component={Contact} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
