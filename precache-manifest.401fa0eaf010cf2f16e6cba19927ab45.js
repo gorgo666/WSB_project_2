f41583b11e1c10e157caeaf78b54065fe6bc0005
gorgo666 <grzegorz.wiecek.sluzbowy@gmail.com>
2020-05-31 11:39:45 +0200

    Updates

---
+++ precache-manifest.401fa0eaf010cf2f16e6cba19927ab45.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a91d1b1dd0b9cb13466c3e6aba1585e",
+    "url": "/WSB_project_2/index.html"
+  },
+  {
+    "revision": "d6e6b9aa9b179dc56471",
+    "url": "/WSB_project_2/static/css/main.72210f50.chunk.css"
+  },
+  {
+    "revision": "ea998d729741cb152a6f",
+    "url": "/WSB_project_2/static/js/2.50b51b54.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/WSB_project_2/static/js/2.50b51b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e6b9aa9b179dc56471",
+    "url": "/WSB_project_2/static/js/main.568564e4.chunk.js"
+  },
+  {
+    "revision": "9f1781ef5dec9787c1e2",
+    "url": "/WSB_project_2/static/js/runtime-main.363ea092.js"
+  },
+  {
+    "revision": "f40e3d20deafc9597ecedf776e38b6c3",
+    "url": "/WSB_project_2/static/media/Excel medium.f40e3d20.pdf"
+  },
+  {
+    "revision": "475772c6848ded5079adc43f4672954c",
+    "url": "/WSB_project_2/static/media/MS SQL.475772c6.pdf"
+  },
+  {
+    "revision": "c5b98c863758c70d9f459d19392434ae",
+    "url": "/WSB_project_2/static/media/VBA advance.c5b98c86.pdf"
+  },
+  {
+    "revision": "30f8d7ef8e1b21d802f2db3ddae6a069",
+    "url": "/WSB_project_2/static/media/VBA przkrojowe.30f8d7ef.pdf"
+  },
+  {
+    "revision": "1468100e0001cb68893356dcdb33a4d0",
+    "url": "/WSB_project_2/static/media/access advance.1468100e.pdf"
+  },
+  {
+    "revision": "84e8cec9cbb3e75b1ffd8690fb9c13d1",
+    "url": "/WSB_project_2/static/media/access basic.84e8cec9.pdf"
+  },
+  {
+    "revision": "aa6c27f7ce53a55069b0d1cce8c95958",
+    "url": "/WSB_project_2/static/media/excel - expert.aa6c27f7.pdf"
+  },
+  {
+    "revision": "e669b90fa29a2f50e0050ebb03afb078",
+    "url": "/WSB_project_2/static/media/gory.e669b90f.jpg"
+  },
+  {
+    "revision": "f042853ad35e3ffe85b7b233e8aaf177",
+    "url": "/WSB_project_2/static/media/img1.f042853a.png"
+  },
+  {
+    "revision": "d0f4655cc599bc11a8b68044c5bb4d05",
+    "url": "/WSB_project_2/static/media/koncerty.d0f4655c.jpg"
+  },
+  {
+    "revision": "86af0db7665f7f5dfa76b43972a7a602",
+    "url": "/WSB_project_2/static/media/power pivot, view, query.86af0db7.pdf"
+  },
+  {
+    "revision": "fba2e510c43c9b2d495ceb21887941ba",
+    "url": "/WSB_project_2/static/media/programowanie.fba2e510.jpg"
+  },
+  {
+    "revision": "0263d2a3960bf41972cb91c0b2b5f3fa",
+    "url": "/WSB_project_2/static/media/przyroda.0263d2a3.jpg"
+  },
+  {
+    "revision": "3c357b2dd877153ec7762e7d4ad74df6",
+    "url": "/WSB_project_2/static/media/pwr_logo.3c357b2d.jpg"
+  },
+  {
+    "revision": "412ff35c423ade5db85a9d4a0e977d5b",
+    "url": "/WSB_project_2/static/media/python1.412ff35c.pdf"
+  }
+]);
